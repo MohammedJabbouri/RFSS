04c7d212491684eff1b34e05a948e76bfde3a1fc
MohammedJabbouri <mohammedbelmeharzia@gmail.com>
2026-08-27 09:14:51 -0700

    Implement main function for EFieldPoint simulation
    Added main function to simulate electric field points with defined parameters.

--- engine/antenna/polarity.c
+++ engine/antenna/polarity.c
@@ -2,4 +2,29 @@
 #include <complex.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include polarity.h
+#include "polarity.h"
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+typedef structure {
+  double t;
+  double Ex;
+  double Ey;
+} EFieldPoint;
+
+int main(){
+  int steps = 2000;
+  double m_time = 2.0 * M_PI;
+  double dt = m_time/steps;
+  
+  double Ax = 1;
+  double Ay = 1;
+
+  double phi_linear = 0;
+  double phi_rhcp = -M_PI / 2;
+  double phi_lhcp = M_PI / 2;
+
+  return 0;
+}
