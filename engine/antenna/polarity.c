9de4645d87c463bd258727d4a4b277de9ddc0d62
MohammedJabbouri <mohammedbelmeharzia@gmail.com>
2026-08-27 18:32:37 -0700

    Fix struct definition and main function formatting

--- engine/antenna/polarity.c
+++ engine/antenna/polarity.c
@@ -8,23 +8,23 @@
 #define M_PI 3.14159265358979323846
 #endif
 
-typedef structure {
-  double t;
-  double Ex;
-  double Ey;
+typedef struct { 
+    double t; 
+    double Ex; 
+    double Ey; 
 } EFieldPoint;
 
-int main(){
-  int steps = 2000;
-  double m_time = 2.0 * M_PI;
-  double dt = m_time/steps;
+int main(void) {
+    int steps = 2000;
+    double m_time = 2.0 * M_PI;
+    double dt = m_time / steps;
   
-  double Ax = 1;
-  double Ay = 1;
-
-  double phi_linear = 0;
-  double phi_rhcp = -M_PI / 2;
-  double phi_lhcp = M_PI / 2;
-
-  return 0;
+    double Ax = 1;
+    double Ay = 1;
+  
+    double phi_linear = 0;
+    double phi_rhcp = -M_PI / 2;
+    double phi_lhcp = M_PI / 2;
+    
+    return 0;
 }
